46a365b7b8eb0020991e8f7735294e0676fa5d15
sdrpsps <z826700@gmail.com>
2022-12-05 18:14:18 +0800

    add: 捕获异常

--- react05/src/App.js
+++ react05/src/App.js
@@ -1,23 +1,36 @@
 import React, { useState, useEffect } from 'react';
-
 import StudentList from './components/StudentList';
 
 function App() {
   // http://localhost:1337/api/students
   // 组件初始化请求数据, useEffect 第二参数为空数组表示只在初始化时执行
   useEffect(() => {
-    fetch('http://localhost:1337/api/students')
+    setLoading(true);
+    fetch('http://localhost:1337/api/student')
       .then((res) => {
+        if (!res.ok) {
+          setLoading(false);
+          throw new Error('数据加载失败');
+        }
         return res.json();
       })
       .then((data) => {
         setStuData(data.data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        setError(true);
+        console.log(err);
       });
   }, []);
   const [stuData, setStuData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
   return (
     <div className="App">
-      <StudentList stus={stuData} />
+      {loading && <p>数据加载中</p>}
+      {error && <p>服务异常</p>}
+      {!loading && !error && <StudentList stus={stuData} />}
     </div>
   );
 }
